fb82673e9cc21d10d71e100b01209aa2fafbb8b7
sging20 <sging20@stuy.edu>
2020-12-07 00:49:04 -0500

    Made next method

--- Range.java
+++ Range.java
@@ -1,17 +1,21 @@
   import java.util.NoSuchElementException;
   public class Range implements IntegerSequence{
   private int start,end,current;
+
   public Range(int startinput,  int endinput){
       start= startinput-1;
       end= endinput;
       current = start;
     }
+
   public void reset(){
       current = start;
     }
+
     public int length(){
         return (Math.abs(start- end));
       }
+
     public boolean hasNext(){
         if(current < end){
           return true;
@@ -20,4 +24,14 @@
           return false;
         }
       }
+      
+    public int next(){
+      if(hasNext()){
+            current++;
+          return current;
+        }
+      else{
+        throw new NoSuchElementException();
+      }
+    }       //return the current value in the sequence and advances to the next element.
 }
