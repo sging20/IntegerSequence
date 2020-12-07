d8ebe83862adae4429bd4e8b66a7ab0fc8357199
sging20 <sging20@stuy.edu>
2020-12-07 00:41:42 -0500

    private int and constuctor

--- Range.java
+++ Range.java
@@ -6,28 +6,5 @@
       end= endinput;
       current = start;
     }
-  public void reset(){
-      current = start;
-    }
-  public int length(){
-      return (Math.abs(start- end));
-    }
-  public boolean hasNext(){
-      if(current < end){
-        return true;
-      }
-      else{
-        return false;
-      }
-    }
-  public int next(){
-    if(hasNext()){
-          current++;
-        return current;
-      }
-    else{
-      throw new NoSuchElementException();
-    }
-  }       //return the current value in the sequence and advances to the next element.
 
 }
