a36459ea2f0d56ba2ce81fd196c1ccd52c344f19
sging20 <sging20@stuy.edu>
2021-01-03 18:49:05 -0500

    fixed constructors

--- ArraySequence.java
+++ ArraySequence.java
@@ -20,26 +20,29 @@ public class ArraySequence implements IntegerSequence{
     if(hasNext() == false){
       throw new NoSuchElementException("No element next.");
   }
-  if(hasNext()== true){
-    this.currentIndex+= 1;
-    ret= this.data[this.currentIndex- 1];
-  }
+  ret= data[currentIndex];
+  currentIndex++;
   return ret;
 }
        //return the current value in the sequence and advances to the next element.
   public int length(){
     return (this.data.length);
   }     //returns the total length of the sequence
+
   public void reset(){
     this.currentIndex= 0;
   }     //start over from the start of the sequence
+
   public ArraySequence(IntegerSequence otherseq){
-    this.data = new int[otherseq.length()];
-    for(int i=0; i<= otherseq.length(); i++){
-      this.data[i]= otherseq.next();
-  }
     otherseq.reset();
+    data= new int[otherseq.length()];
+    int index=0;
+    while(otherseq.hasNext()){
+        data[index]= otherseq.next();
+        index++;
     }
+    otherseq.reset();
+  }
 
 
 }
